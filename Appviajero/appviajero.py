7c691f575f78e3db5eba7f8fefabf82cdd4faf32
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-04-13 14:49:45 -0500

    Update appviajero.py

--- Appviajero/appviajero.py
+++ Appviajero/appviajero.py
@@ -21,12 +21,12 @@ elif opcion_autenticacion == "Registrarse":
     crear_usuario(usuarios)
 
 # Sección "Acerca de mí"
-    st.header("Acerca de Mí")
-    st.write('''Mi nombre es Santiago Molina Velasquez y soy estudiante 
+st.header("Acerca de Mí")
+st.write('''Mi nombre es Santiago Molina Velasquez y soy estudiante 
              de ingeniería de sistemas en la Universidad Nacional de Colombia. 
              Me apasiona el mundo de la tecnología y estoy comprometido a brindar soluciones 
              innovadoras a través del desarrollo de software y la ingeniería de sistemas. 
              Tengo como metas cercanas terminar mi carrera profesional y seguir 
              consolidandome y laborando en todo lo relacionado a tecnología y 
              en el gran mundo del internet.''')
-    st.write("Puedes contactarme en smolinav@unal.edu.co")
+st.write("Puedes contactarme en smolinav@unal.edu.co")
