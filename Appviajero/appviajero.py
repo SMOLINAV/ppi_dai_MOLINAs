64971aba0a4c06a6fdf066c69017f5c962f62017
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-04-13 13:57:23 -0500

    nuevo intento con userinfo

--- Appviajero/appviajero.py
+++ Appviajero/appviajero.py
@@ -13,31 +13,10 @@ opcion = st.radio("Selecciona una opción:", ("Crear un nuevo usuario", "Iniciar
 
 # Manejo de la opción seleccionada
 if opcion == "Crear un nuevo usuario":
-    st.header("Crear Nuevo Usuario")
-    nuevo_usuario = st.text_input("Nombre de usuario")
-    nueva_contraseña = st.text_input("Contraseña", type="password")
-
-    if st.button("Registrar"):
-        if nuevo_usuario in usuarios:
-            st.error("El usuario ya existe. Por favor, elige otro nombre de usuario.")
-        else:
-            usuarios[nuevo_usuario] = nueva_contraseña
-            st.success("Usuario creado exitosamente. ¡Ahora puedes iniciar sesión!")
+    crear_usuario(usuarios)
 
 elif opcion == "Iniciar sesión":
-    st.header("Iniciar Sesión")
-    usuario = st.text_input("Nombre de usuario")
-    contraseña = st.text_input("Contraseña", type="password")
-
-    if st.button("Iniciar Sesión"):
-        if usuario not in usuarios:
-            st.error("Usuario no encontrado. Por favor, registra una cuenta.")
-        elif usuarios[usuario] != contraseña:
-            st.error("Contraseña incorrecta. Por favor, inténtalo de nuevo.")
-        else:
-            st.success(f"Bienvenido, {usuario}! Has iniciado sesión exitosamente.")
-            # Redirigir al usuario al tablero
-            show_dashboard(usuario)
+    iniciar_sesion(usuarios)
 
 # Mostrar información del usuario
 if st.button("Mostrar Información del Usuario"):
