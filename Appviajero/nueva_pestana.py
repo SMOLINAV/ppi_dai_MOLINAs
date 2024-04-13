2462425f59fa01b14350ea660b4bb0a071f4ede9
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-04-13 15:39:54 -0500

    jum

--- Appviajero/nueva_pestana.py
+++ Appviajero/nueva_pestana.py
@@ -2,5 +2,11 @@ import streamlit as st
 
 def mostrar_pagina_busqueda():
     st.title("Busca tu próximo destino")
-    st.header("Búsqueda")
-    # Agrega aquí el código para el buscador u otras funcionalidades que desees
+    
+    # Elemento para escribir el próximo destino
+    proximo_destino = st.text_input("Escribe tu próximo destino")
+
+    # Botón para realizar la búsqueda
+    if st.button("Buscar"):
+        # Aquí puedes agregar la lógica para realizar la búsqueda del destino
+        st.write(f"Buscando información sobre {proximo_destino}...")
