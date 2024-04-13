aa4b9af6779353a2f515f0793d4779d89c6a4231
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-04-13 14:39:42 -0500

    Update appviajero.py

--- Appviajero/appviajero.py
+++ Appviajero/appviajero.py
@@ -9,8 +9,8 @@ usuarios = {"usuario1": "password1", "usuario2": "password2"}
 st.title("APP VIAJEROFELIZ - Planificador de Viajes")
 
 # Sección de "Iniciar Sesión" o "Registrarse"
-st.sidebar.title("Autenticación")
-opcion_autenticacion = st.sidebar.radio("Selecciona una opción:", ("Iniciar Sesión", "Registrarse"))
+st.header("Autenticación")
+opcion_autenticacion = st.radio("Selecciona una opción:", ("Iniciar Sesión", "Registrarse"))
 
 if opcion_autenticacion == "Iniciar Sesión":
     usuario = iniciar_sesion(usuarios)
@@ -21,5 +21,5 @@ elif opcion_autenticacion == "Registrarse":
     crear_usuario(usuarios)
 
 # Sección "Acerca de mí"
-st.sidebar.title("Acerca de")
-st.sidebar.write("Mi nombre es Santiago Molina Velasquez. Soy estudiante de ingeniería de sistemas en la Universidad Nacional de Colombia. Me apasiona el mundo de la tecnología y estoy comprometido a brindar soluciones innovadoras a través del desarrollo de software y la ingeniería de sistemas.")
+st.header("Acerca de")
+st.write("Mi nombre es Santiago Molina Velasquez. Soy estudiante de ingeniería de sistemas en la Universidad Nacional de Colombia. Me apasiona el mundo de la tecnología y estoy comprometido a brindar soluciones innovadoras a través del desarrollo de software y la ingeniería de sistemas.")
