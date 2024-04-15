931df6fb1c305190398f21b482fa2547795030f6
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-04-15 10:25:50 -0500

    Delete dashboard.py

--- Appviajero/dashboard.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def show_dashboard(usuario):
-    st.title(f"Bienvenido al Tablero, {usuario}!")
-    st.write("Aquí puedes disfrutar de todas las funcionalidades de la aplicación.")
-    # Agrega aquí todas las funcionalidades del tablero que desees
