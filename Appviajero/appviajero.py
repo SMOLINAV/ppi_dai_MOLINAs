ca86cbca5b7818cc939cf3b2dba6a9d76acbdc29
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-05-19 17:11:43 -0500

    inicio

--- Appviajero/appviajero.py
+++ Appviajero/appviajero.py
@@ -43,10 +43,64 @@ if st.session_state.aceptado:
     opcion_autenticacion = st.sidebar.selectbox("Selecciona una opción:", ["Inicio", "iniciar sesión", "Registrarse", "Cambiar Contraseña"])
     # Procesar la opción seleccionada
     if opcion_autenticacion == "Inicio":
-        usuario = iniciar_sesion()
-        if usuario:
-            st.session_state.usuario = usuario  # Guardar el usuario en la sesión
+        # Título de la aplicación
+        st.title("APP VIAJEROFELIZ")
 
+
+        # Seccion buscar lugares Colombia
+        st.header("Buscar Lugares En Colombia")
+        ciudad = st.text_input("Ingrese el nombre de una ciudad colombiana:")
+        if ciudad:
+            lugares = buscar_lugares_ciudad(ciudad)
+            st.write(f"Lugares bonitos e importantes de {ciudad.capitalize()}:")
+            for lugar in lugares:
+                st.write(f"- {lugar}")
+
+
+        # Seccion buscar lugares paises América
+        st.header("Buscar Lugares En América")
+        if "usuario" in st.session_state:
+            paises = [
+                "Argentina", "Brasil", "Canadá", "Chile", "Costa Rica", "Cuba",
+                "Estados Unidos", "México",
+                "Panamá", "Paraguay", "Perú", "Puerto Rico", "Uruguay", "Venezuela"
+            ]
+            pais = st.selectbox("Seleccione un país:", sorted(paises))
+            if pais:
+                ciudades = buscar_lugares_ciudad_pais(pais)
+                st.write(f"Ciudades importantes de {pais.capitalize()}:")
+                for ciudad, lugares in ciudades.items():
+                    st.write(f"{ciudad}:")
+                    for lugar in lugares:
+                        st.write(f"- {lugar}")
+
+        else:
+            st.write("Por favor, inicia sesión para acceder a la función de busqueda en paises de América.")
+
+
+        # Sección para ver el precio de vuelos
+        st.header("Ver Precio De Vuelos")
+        if "usuario" in st.session_state:
+            if st.button("Ver Precio de Vuelos en Avión"):
+                st.write("Encontrarás el link para ir a una pagina de google, para poner tu lugar de destino y saber los vuelos que hay disponibles y sus precios")
+                st.markdown("https://www.google.com/travel/flights?hl=es")
+        else:
+            st.write("Por favor, inicia sesión para acceder a la función para calcular el precio de vuelos.")
+
+
+        # Sección "Acerca de mí"
+        st.write("<span style='color:green'>Acerca De Mí</span>", unsafe_allow_html=True)
+        st.write('''Mi nombre es Santiago Molina Velasquez y soy estudiante 
+                    de ingeniería de sistemas en la Universidad Nacional de Colombia. 
+                    Me apasiona el mundo de la tecnología y estoy comprometido a brindar soluciones 
+                    innovadoras a través del desarrollo de software y la ingeniería de sistemas. 
+                    Tengo como metas cercanas terminar mi carrera profesional y seguir 
+                    consolidándome y laborando en todo lo relacionado a tecnología y 
+                    en el gran mundo del internet.''')
+        st.write("Puedes contactarme smolinav@unal.edu.co")
+
+    elif opcion_autenticacion == "iniciar sesión":
+        auth.login_user()
     elif opcion_autenticacion == "Registrarse":
         crear_usuario()
 
@@ -55,58 +109,3 @@ if st.session_state.aceptado:
             cambiar_contraseña(st.session_state.usuario)
 
 
-    # Título de la aplicación
-    st.title("APP VIAJEROFELIZ")
-
-
-    # Seccion buscar lugares Colombia
-    st.header("Buscar Lugares En Colombia")
-    ciudad = st.text_input("Ingrese el nombre de una ciudad colombiana:")
-    if ciudad:
-        lugares = buscar_lugares_ciudad(ciudad)
-        st.write(f"Lugares bonitos e importantes de {ciudad.capitalize()}:")
-        for lugar in lugares:
-            st.write(f"- {lugar}")
-
-
-    # Seccion buscar lugares paises América
-    st.header("Buscar Lugares En América")
-    if "usuario" in st.session_state:
-        paises = [
-            "Argentina", "Brasil", "Canadá", "Chile", "Costa Rica", "Cuba",
-            "Estados Unidos", "México",
-            "Panamá", "Paraguay", "Perú", "Puerto Rico", "Uruguay", "Venezuela"
-        ]
-        pais = st.selectbox("Seleccione un país:", sorted(paises))
-        if pais:
-            ciudades = buscar_lugares_ciudad_pais(pais)
-            st.write(f"Ciudades importantes de {pais.capitalize()}:")
-            for ciudad, lugares in ciudades.items():
-                st.write(f"{ciudad}:")
-                for lugar in lugares:
-                    st.write(f"- {lugar}")
-
-    else:
-        st.write("Por favor, inicia sesión para acceder a la función de busqueda en paises de América.")
-
-
-    # Sección para ver el precio de vuelos
-    st.header("Ver Precio De Vuelos")
-    if "usuario" in st.session_state:
-        if st.button("Ver Precio de Vuelos en Avión"):
-            st.write("Encontrarás el link para ir a una pagina de google, para poner tu lugar de destino y saber los vuelos que hay disponibles y sus precios")
-            st.markdown("https://www.google.com/travel/flights?hl=es")
-    else:
-        st.write("Por favor, inicia sesión para acceder a la función para calcular el precio de vuelos.")
-
-
-    # Sección "Acerca de mí"
-    st.write("<span style='color:green'>Acerca De Mí</span>", unsafe_allow_html=True)
-    st.write('''Mi nombre es Santiago Molina Velasquez y soy estudiante 
-                de ingeniería de sistemas en la Universidad Nacional de Colombia. 
-                Me apasiona el mundo de la tecnología y estoy comprometido a brindar soluciones 
-                innovadoras a través del desarrollo de software y la ingeniería de sistemas. 
-                Tengo como metas cercanas terminar mi carrera profesional y seguir 
-                consolidándome y laborando en todo lo relacionado a tecnología y 
-                en el gran mundo del internet.''')
-    st.write("Puedes contactarme smolinav@unal.edu.co")
--- Appviajero/auth.py
+++ Appviajero/auth.py
@@ -1,91 +1,72 @@
 import streamlit as st # type: ignore
-import json
+registered_users = {}
 
-def cargar_usuarios():
-    '''
-    esta función carga los usuarios existentes en el archivo 'usuarios.json'
-    y los devuelve como un diccionario.
-    si el archivo no existe, devuelve un diccionario vacío.
-    args:
-        None
-    returns:
-        dict: diccionario de usuarios
-    '''
-    try:
-        with open("usuarios.json", "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
+def register_user():
+    """
+    Registra un nuevo usuario en el sistema.
+    
+    Args:
+        new_username (str): El nombre de usuario del nuevo usuario.
+        new_password (str): La contraseña del nuevo usuario.
 
-def guardar_usuarios(usuarios):
-    '''
-    esta función escribe los usuarios en el archivo 'usuarios.json'
-    args:
-        dict: diccionario de usuarios
-    returns:
+    Returns:
         None
-    '''
-    with open("usuarios.json", "w") as f:
-        json.dump(usuarios, f)
-
-def crear_usuario():
-    '''
-    esta función crea un nuevo usuario en el archivo 'usuarios.json'
-    args:
-        None
-    returns:
-        None
-    '''
-    st.header("Crear Nuevo Usuario")
-    nuevo_usuario = st.text_input("Nombre de usuario")
-    nueva_contraseña = st.text_input("Contraseña", type="password")
-
-    if st.button("Registrar"):
-        usuarios = cargar_usuarios()
-        if nuevo_usuario in usuarios:
-            st.error("El usuario ya existe. Por favor, elige otro nombre de usuario.")
+    """
+    st.subheader("Registro de usuario")
+    new_username = st.text_input("Nuevo usuario")
+    new_password = st.text_input("Nueva contraseña", type="password")
+    if st.button("Registrarse"):
+        if new_username in registered_users:
+            st.error("El usuario ya existe. Por favor, elige otro.")
         else:
-            usuarios[nuevo_usuario] = nueva_contraseña
-            guardar_usuarios(usuarios)
-            st.success("Usuario creado exitosamente. ¡Ahora puedes iniciar sesión!")
+            registered_users[new_username] = new_password
+            st.success("Usuario registrado exitosamente. Por favor,\
+            inicia sesión.")
 
-def iniciar_sesion():
-    '''
-    esta función inicia la sesión de un usuario existente
-    args:
+
+def change_password():
+    """
+    Cambia la contraseña de un usuario existente.
+    
+    Args: None
+
+    Returns:
         None
-    returns:
-        str: nombre de usuario
-    '''
-    usuarios = cargar_usuarios()
-    st.header("Iniciar Sesión")
-    usuario = st.text_input("Nombre de usuario")
-    contraseña = st.text_input("Contraseña", type="password")
+    """
+    st.subheader("Cambio de contraseña")
+    user = st.text_input("Usuario:")
+    contraseña_actual = st.text_input("Contraseña actual:", type="password")
+    nueva_contraseña = st.text_input("Nueva contraseña:", type="password")
 
-    if st.button("Iniciar Sesión"):
-        if usuario not in usuarios:
-            st.error("Usuario no encontrado. Por favor, registra una cuenta.")
-        elif usuarios[usuario] != contraseña:
-            st.error("Contraseña incorrecta. Por favor, inténtalo de nuevo.")
+    if st.button("Actualizar"):
+        if user in registered_users:
+            if registered_users[user] == contraseña_actual:
+                registered_users[user] = nueva_contraseña
+                st.success("¡Contraseña actualizada con éxito!")
+            else:
+                st.error("Contraseña incorrecta")
         else:
-            st.success(f"Bienvenido, {usuario}! Has iniciado sesión exitosamente.")
-            return usuario
+            st.error("Usuario no registrado")
 
-def cambiar_contraseña(usuario):
-    '''
-    esta función cambia la contraseña de un usuario existente
-    args:
-        str: nombre de usuario
-    returns:
-        None
-    '''
-    usuarios = cargar_usuarios()
-    if usuario in usuarios:
-        st.header("Cambio de Contraseña")
-        nueva_contraseña = st.text_input("Nueva Contraseña", type="password")
-        if st.button("Cambiar Contraseña"):
-            usuarios[usuario] = nueva_contraseña
-            guardar_usuarios(usuarios)
-            st.success("Contraseña actualizada exitosamente.")
-    else:
-        st.error("Usuario no encontrado. Por favor, inicia sesión nuevamente.")
+def login_user():
+    """
+    Iniciar sesión de usuario.
+
+    Args: None'
+
+    Returns:  True si el inicio de sesión fue exitoso, False en caso contrario.
+    """
+    st.subheader("Iniciar sesión")
+    username = st.text_input("Usuario")
+    password = st.text_input("Contraseña", type="password")
+    if st.button("Iniciar sesión"):
+        if username in registered_users:
+            if registered_users[username] == password:
+                st.success("Inicio de sesión exitoso. ¡Bienvenido, {}!"
+                .format(username))    
+                return True
+            else:
+                st.error("Contraseña incorrecta")
+        else:
+            st.error("Usuario no registrado")
+    return False
