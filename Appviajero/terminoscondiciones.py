47e18e7b524f5317965e20fabb01227933e09ef2
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-05-19 16:00:20 -0500

    Create terminoscondiciones.py

---
+++ Appviajero/terminoscondiciones.py
@@ -0,0 +1,216 @@
+import streamlit as st # type: ignore
+def terminos_condiciones():
+    """ 
+    Botón para abrir la ventana emergente para política
+    
+    Args: None
+
+    Returns: None
+    """
+    st.title("Términos y Condiciones")
+
+    st.write('''
+Términos y condiciones
+Última actualización: 28 de abril de 2024
+
+Por favor, lea atentamente estos términos y condiciones antes de utilizar Nuestro Servicio.\
+
+Interpretación y definiciones
+Interpretación
+Las palabras cuyas letras iniciales se escriben en mayúscula tienen significados definidos\
+bajo las siguientes condiciones. Las siguientes definiciones tendrán el mismo significado\
+independientemente de que aparezcan en singular o en plural.
+
+Definiciones
+A los efectos de estos Términos y Condiciones:
+
+Filial significa una entidad que controla, es controlada por o está bajo control común con\
+una parte, donde "control" significa la propiedad del 50% o más de las acciones, \
+participaciones en el capital u otros valores con derecho a voto para la elección de\
+             directores u otra autoridad administrativa.
+
+País se refiere a: Colombia
+
+La Compañía (denominada "la Compañía", "Nosotros", "Nos" o "Nuestro" en este Acuerdo)\
+              se refiere a appviajero.
+
+Dispositivo se refiere a cualquier dispositivo que pueda acceder al Servicio, como\
+             una computadora, un teléfono celular o una tableta digital.
+
+El servicio se refiere al sitio web.
+
+Términos y Condiciones (también denominados "Términos") se refieren a estos Términos\
+              y Condiciones que forman el acuerdo completo entre Usted y la Compañía\
+             con respecto al uso del Servicio. Este acuerdo de Términos y Condiciones\
+             ha sido creado con la ayuda del Generador de Términos y Condiciones Gratuito.
+
+Servicio de redes sociales de terceros se refiere a cualquier servicio o contenido \
+             (incluidos datos, información, productos o servicios) proporcionado por un\
+             tercero que pueda ser mostrado, incluido o puesto a disposición por el Servicio.
+
+Sitio web se refiere a appviajero, accesible desde https://appviajero.streamlit.app/
+
+Usted se refiere a la persona que accede o utiliza el Servicio, o a la empresa u otra\
+entidad legal en nombre de la cual dicha persona accede o utiliza el Servicio, según\
+             corresponda.
+
+Reconocimiento
+Estos son los Términos y Condiciones que rigen el uso de este Servicio y el acuerdo\
+             que opera entre Usted y la Compañía. Estos Términos y Condiciones establecen\
+              los derechos y obligaciones de todos los usuarios con respecto al\
+             uso del Servicio.
+
+Su acceso y uso del Servicio está condicionado a su aceptación y cumplimiento de estos\
+             Términos y Condiciones. Estos Términos y Condiciones se aplican a todos\
+             los visitantes, usuarios y otras personas que acceden o utilizan el Servicio.
+
+Al acceder o utilizar el Servicio, Usted acepta estar sujeto a estos Términos y Condiciones.\
+ Si no está de acuerdo con alguna parte de estos Términos y Condiciones, no podrá\
+             acceder al Servicio.
+
+Usted declara que es mayor de 18 años. La Compañía no permite que los menores de 18\
+             años utilicen el Servicio.
+
+Su acceso y uso del Servicio también está condicionado a su aceptación y cumplimiento\
+             de la Política de Privacidad de la Compañía. Nuestra Política de privacidad\
+             describe nuestras políticas y procedimientos sobre la recopilación, el uso y\
+                a divulgación de su información personal cuando utiliza la Aplicación o el\
+             sitio web y le informa sobre sus derechos de privacidad y cómo la ley lo protege.\
+    Por favor, lea atentamente nuestra Política de Privacidad antes de utilizar Nuestro Servicio.
+
+Enlaces a otros sitios web
+Nuestro Servicio puede contener enlaces a sitios web o servicios de terceros que no\
+             son propiedad ni están controlados por la Compañía.
+
+La Compañía no tiene control ni asume ninguna responsabilidad por el contenido, las\
+             políticas de privacidad o las prácticas de los sitios web o servicios de\
+             terceros. Además, reconoce y acepta que la Compañía no será responsable,\
+             directa o indirectamente, de ningún daño o pérdida causada o presuntamente\
+             causada por o en relación con el uso o la confianza en dicho contenido, bienes\
+             o servicios disponibles en o a través de dichos sitios web o servicios.
+
+Le recomendamos encarecidamente que lea los términos y condiciones y las políticas de\
+             privacidad de cualquier sitio web o servicio de terceros que visite.
+
+Terminación
+Podemos cancelar o suspender su acceso de inmediato, sin previo aviso ni responsabilidad,\
+             por cualquier motivo, incluidos, entre otros, si incumple estos Términos y condiciones.
+
+Tras la rescisión, su derecho a utilizar el Servicio cesará inmediatamente.
+
+Limitación de responsabilidad
+Sin perjuicio de los daños en los que pueda incurrir, la responsabilidad total de la\
+             Compañía y cualquiera de sus proveedores en virtud de cualquier disposición de\
+             estos Términos y Su recurso exclusivo por todo lo anterior se limitará a la\
+             cantidad realmente pagada por Usted a través del Servicio o 100 USD si no ha\
+             comprado nada a través del Servicio.
+
+En la medida máxima permitida por la ley aplicable, en ningún caso la Compañía o sus\
+             proveedores serán responsables de ningún daño especial, incidental, indirecto\
+             o consecuente de ningún tipo (incluidos, entre otros, daños por pérdida de\
+             ganancias, pérdida de datos u otra información, por interrupción del negocio,\
+            por lesiones personales, pérdida de privacidad que surjan o estén relacionados\
+             de alguna manera con el uso o la imposibilidad de usar el Servicio, software\
+             de terceros y/o hardware de terceros utilizado con el Servicio, o de otro modo\
+             en relación con cualquier disposición de estos Términos), incluso si la Compañía\
+             o cualquier proveedor ha sido advertido de la posibilidad de tales daños e incluso\
+             si el recurso no cumple con su propósito esencial.
+
+Algunos estados no permiten la exclusión de garantías implícitas o la limitación de\
+             responsabilidad por daños incidentales o consecuentes, lo que significa que algunas\
+             de las limitaciones anteriores pueden no aplicarse. En estos estados, la\
+             responsabilidad de cada parte se limitará en la mayor medida permitida por la ley.
+
+Descargo de responsabilidad "TAL CUAL" y "SEGÚN DISPONIBILIDAD"
+El Servicio se le proporciona "TAL CUAL" y "SEGÚN DISPONIBILIDAD" y con todas las fallas\
+             y defectos sin garantía de ningún tipo. En la medida máxima permitida por la\
+             ley aplicable, la Compañía, en su propio nombre y en nombre de sus Afiliados\
+             y sus respectivos licenciantes y proveedores de servicios, renuncia expresamente\
+             a todas las garantías, ya sean expresas, implícitas, estatutarias o de otro tipo,\
+             con respecto al Servicio, incluidas todas las garantías implícitas de\
+             comerciabilidad, idoneidad para un propósito particular, título y no infracción.\
+             y las garantías que puedan surgir del curso de la negociación, el curso de la\
+             ejecución, el uso o la práctica comercial. Sin limitación a lo anterior, la Compañía\
+             no ofrece ninguna garantía o compromiso, y no hace ninguna declaración de ningún\
+             tipo de que el Servicio cumplirá con sus requisitos, logrará los resultados\
+             previstos, será compatible o funcionará con cualquier otro software,\
+             plicaciones, sistemas o servicios, funcionará sin interrupción, cumplirá\
+             con cualquier estándar de rendimiento o confiabilidad o estará libre de\
+             errores o que cualquier error o defecto puede o será corregido.
+
+Sin perjuicio de lo anterior, ni la Compañía ni ninguno de los proveedores de la\
+             Compañía hacen ninguna representación o garantía de ningún tipo, expresa\
+             o implícita: (i) en cuanto a la operación o disponibilidad del Servicio,\
+             o la información, el contenido y los materiales o productos incluidos en\
+             el mismo; (ii) que el Servicio será ininterrumpido o estará libre de errores;\
+             (iii) en cuanto a la exactitud, confiabilidad o vigencia de cualquier información\
+             o contenido proporcionado a través del Servicio; o (iv) que el Servicio, sus\
+             servidores, el contenido o los correos electrónicos enviados desde o en nombre\
+             de la Compañía están libres de virus, scripts, troyanos, gusanos, malware, bombas\
+             de tiempo u otros componentes dañinos.
+
+Algunas jurisdicciones no permiten la exclusión de ciertos tipos de garantías o limitaciones\
+             sobre los derechos legales aplicables de un consumidor, por lo que es posible\
+             que algunas o todas las exclusiones y limitaciones anteriores no se apliquen en\
+             su caso. Pero en tal caso, las exclusiones y limitaciones establecidas en esta\
+             sección se aplicarán en la mayor medida posible según la ley aplicable.
+
+Legislación aplicable
+Las leyes del País, excluyendo sus normas sobre conflictos de leyes, regirán estos Términos\
+             y Su uso del Servicio. Su uso de la Aplicación también puede estar sujeto a\
+             otras leyes locales, estatales, nacionales o internacionales.
+
+Resolución de disputas
+Si tiene alguna inquietud o disputa sobre el Servicio, acepta primero intentar resolver\
+             la disputa de manera informal poniéndose en contacto con la Compañía.
+
+Para usuarios de la Unión Europea (UE)
+Si usted es un consumidor de la Unión Europea, se beneficiará de las disposiciones\
+             obligatorias de la ley del país en el que reside.
+
+Cumplimiento legal de los Estados Unidos
+Usted declara y garantiza que (i) no se encuentra en un país que esté sujeto al embargo\
+             del gobierno de los Estados Unidos, o que haya sido designado por el gobierno\
+             de los Estados Unidos como un país que "apoya el terrorismo", y (ii) no figura\
+             en ninguna lista del gobierno de los Estados Unidos de partes prohibidas o restringidas.
+
+Divisibilidad y renuncia
+Divisibilidad
+Si alguna disposición de estos Términos se considera inaplicable o inválida, dicha disposición\
+             se modificará e interpretará para lograr los objetivos de dicha disposición en\
+             la mayor medida posible según la ley aplicable y las disposiciones restantes\
+             continuarán en pleno vigor y efecto.
+
+Renuncia
+Salvo lo dispuesto en el presente documento, el hecho de no ejercer un derecho o exigir\
+             el cumplimiento de una obligación en virtud de estos Términos no afectará a\
+             la capacidad de una parte para ejercer dicho derecho o exigir dicho cumplimiento\
+             en cualquier momento posterior, ni la renuncia a un incumplimiento constituirá\
+             una renuncia a cualquier incumplimiento posterior.
+
+Traducción Interpretación
+Estos Términos y Condiciones pueden haber sido traducidos si los hemos puesto a su disposición\
+             en nuestro Servicio. Usted acepta que el texto original en inglés prevalecerá\
+             en caso de disputa.
+
+Cambios a estos Términos y Condiciones
+Nos reservamos el derecho, a nuestra entera discreción, de modificar o reemplazar estos\
+             Términos en cualquier momento. Si una revisión es sustancial, haremos todos\
+             los esfuerzos razonables para notificar con al menos 30 días de antelación a\
+             la entrada en vigor de los nuevos términos. Lo que constituye un cambio\
+             sustancial se determinará a nuestra entera discreción.
+
+Al continuar accediendo o utilizando Nuestro Servicio después de que esas revisiones\
+             entren en vigencia, Usted acepta estar sujeto a los términos revisados.\
+             Si no está de acuerdo con los nuevos términos, en su totalidad o en parte,\
+             deje de usar el sitio web y el Servicio.
+
+Contáctenos
+Si tiene alguna pregunta sobre estos Términos y Condiciones, puede ponerse en\
+             contacto con nosotros:
+
+Por correo electrónico: smolinav@unal.edu.co
+
+Visitando esta página de nuestro sitio web: https://github.com/SMOLINAV/ppi_pl_MOLINAs
+
+''')
